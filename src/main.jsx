56c046f365f1f987d51245c00b7defa6e106b8ef
Jan <jandylviosva@gmail.com>
2026-07-20 16:10:44 +0800

    Render ReviewForm on review page and App otherwise

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import ReviewForm from './ReviewForm.jsx';
 
-createRoot(document.getElementById('root')).render(<App/>);
+const isReviewPage = window.location.pathname.replace(/\/$/, '') === '/leave-a-review';
+
+createRoot(document.getElementById('root')).render(isReviewPage ? <ReviewForm/> : <App/>);
